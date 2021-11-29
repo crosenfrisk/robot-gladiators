aadb00c1d7f809f17f91ce174bc7e0b1772bcc93
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2021-11-29 12:36:55 -0600

    adding initial window prompt for user input

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a variable called "playerName"
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+console.log("This log is a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("our robot's name is " + playerName);
+
+// this creates a function named "fight"//
+
+function fight(){
+    window.alert("The fight has begun!");
+}
+
+// fight();
+
+var name = "your name";
+console.log(name);
