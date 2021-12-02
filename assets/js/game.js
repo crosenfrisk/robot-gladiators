5bfc4823a707ffb5f81b09367d54558132186734
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2021-12-02 09:44:23 -0600

    put endGame() inside startGame() function; created playAgainConfirm option after endGame to start again or exit.

--- assets/js/game.js
+++ assets/js/game.js
@@ -99,14 +99,34 @@ var startGame = function() {
     else {
       window.alert('You have lost your robot in battle! Game Over!');
       break;
-    }
+    } 
   }
+  //after the loop ends, player is either out of health or enemies to fight, so run the endGame funcion
+  endGame();
 };
 
 // function to end the entire game
 var endGame = function() {
-  window.alert("The game has now ended. Let's see how you did!");
+  //if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  }
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+
+  //ask player if they'd like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) {
+    //restart the game
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
 };
+
+
 
 // start game when the page loads // play again
 startGame();
